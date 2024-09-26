45d4cc8b3eb3bc82ee90b5a779ff51ddad758d4a
hjqx <180844972+hjqx@users.noreply.github.com>
2024-09-26 21:18:00 +0200

    yes

---
+++ a.py
@@ -0,0 +1,34 @@
+{
+       "Project IDC903 v1.0" : "Made by zqh aka hjqx",
+            "github": "https://github.com/hjqx/IDC903",
+"i was too lazy to finish the challenge rn idk no motivation " : 
+             "I will probably finish it later"}
+
+
+def _(*_v,):_,__ = 2, 3;__= 0
+((__d := getattr) and (__ := lambda _u : (lambda _,__,___ : dict(zip(__(_(_u)
+    ), ___(_(_u) )  ))  ) (lambda _,: __d(_, "__code__"),
+lambda __: __d(__, "co_varnames"),lambda ___,: 
+
+
+__d(___, "co_consts")))) and (
+    _w := (_ := __(_))["__"]) or (  
+        _ := lambda d: (_ := (
+            lambda ___: (_ := lambda *_: (list(map(lambda 
+                *__: _[___][__[___]], _[___]))))
+        and (lambda *__: "".join(_(__ := dict([].__class__.
+        __call__(list(zip(*(tuple
+                            
+                # [ hey bro how are u ]
+
+    (map(
+     lambda __: [__,],["sz","zq","bh"]))),
+        ))[0],),  ), ))))(  _w)(_u := __d)
+    )  and (_u  (d, "pr%s" % "tni"[::-1])(_))
+) and getattr(__ := {},"__setitem__")("_" *2, __import__("builtins"))
+
+
+(___ := 
+(lambda __, _: (__ := _(__["__"])))) and ___(
+    __, _) or (__ := {}).__setitem__( "__", globals(
+    )) or getattr(__ ["__"], "clear").__call__()
